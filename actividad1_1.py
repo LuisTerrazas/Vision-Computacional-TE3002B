237ce19d32f9af9d124221c697a6f4f2face5646
LuisTerrazas <A01377440@tec.mx>
2022-04-25 21:29:41 -0500

    "Comentarios"

--- actividad1_1.py
+++ actividad1_1.py
@@ -1,18 +1,19 @@
 import cv2 as cv
 import numpy as np
 
+#Loop para lectura de imagenes de edificios
 for i in range(5):
+    #Lectura de imagnes y cracion de imagenes en grises, lineas y contornos
     img = cv.imread(str(f'Images_Building/img{i}.jpg'))
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     edges = cv.Canny(gray,50,150,apertureSize=3)
     lines = cv.HoughLinesP(edges,1,np.pi/180,100,minLineLength=100, maxLineGap=10)
 
-
-
     canny = cv.Canny(gray, 240, 240)
     contours, _ = cv.findContours(canny, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
+    #Mostrar imagenes con diferentes detecciones de bordes
     cv.imshow("Canny", canny)
 
     cv.waitKey(0)
@@ -25,6 +26,7 @@ for i in range(5):
     cv.waitKey(0)
     cv.destroyAllWindows()
 
+    #Loop para lineas de hough
     for line in lines:
         x1,y1,x2,y2 = line[0]
         cv.line(img,(x1,y1),(x2,y2),(0,255,0),2)
@@ -36,6 +38,7 @@ for i in range(5):
 
     list_Border = [canny]
 
+    #Implementacion de morfologias
     for j in list_Border:
         # Kernel Definition
         kernel = np.ones((5, 5), np.uint8)
@@ -56,6 +59,7 @@ for i in range(5):
 
         list_Morphological = [erosion, dilation, opening, closing, gradient, tophat, blackhat]
 
+        #Mostrar imagenes con morfologias concatenadas
         Morpho = np.concatenate(list_Morphological, axis=1)
         Morpho_Resize = cv.resize(Morpho,(1800,250), interpolation=cv.INTER_AREA)
         cv.imshow("Morphological(erosion, dilation, opening, closing, gradient, tophat, blackhat)", Morpho_Resize)
