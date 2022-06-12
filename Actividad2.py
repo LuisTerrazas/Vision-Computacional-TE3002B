7f059936dd9d161a99ae2381f3b30d177ca624e5
LuisTerrazas <A01377440@tec.mx>
2022-06-11 19:52:21 -0500

    "Actividad2_ver01"

--- Actividad2.py
+++ Actividad2.py
@@ -1,71 +1,65 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+from skimage.filters import threshold_otsu
 
 for i in range(10):
-
     img = cv2.imread(str(f'Images_sign/img{i}.png'))
-    b, g, r = cv2.split(img)
-    rgb_img = cv2.merge([r, g, b])
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-    # noise removal
-    kernel = np.ones((2, 2), np.uint8)
-    # opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
-    closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+    twoDimage = img.reshape((-1, 3))
+    twoDimage = np.float32(twoDimage)
 
-    # sure background area
-    sure_bg = cv2.dilate(closing, kernel, iterations=3)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 
-    # Finding sure foreground area
-    dist_transform = cv2.distanceTransform(sure_bg, cv2.DIST_L2, 3)
+    ret, label, center = cv2.kmeans(twoDimage, 2, None, criteria, 50, cv2.KMEANS_PP_CENTERS)
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    result_image = res.reshape(img.shape)
 
-    # Threshold
-    ret, sure_fg = cv2.threshold(dist_transform, 0.1 * dist_transform.max(), 255, 0)
+    cv2.imshow("Kmeans", result_image)
 
-    # Finding unknown region
-    sure_fg = np.uint8(sure_fg)
-    unknown = cv2.subtract(sure_bg, sure_fg)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
 
-    # Marker labelling
-    ret, markers = cv2.connectedComponents(sure_fg)
 
-    # Add one to all labels so that sure background is not 0, but 1
-    markers = markers + 1
+    def filter_image(image, mask):
+        r = image[:, :, 0] * mask
+        g = image[:, :, 1] * mask
+        b = image[:, :, 2] * mask
+        return np.dstack([r, g, b])
 
-    # Now, mark the region of unknown with zero
-    markers[unknown == 255] = 0
 
-    markers = cv2.watershed(img, markers)
-    img[markers == -1] = [255, 0, 0]
+    thresh = threshold_otsu(img_gray)
+    img_otsu = img_gray < thresh
+    filtered = filter_image(img, img_otsu)
+    cv2.imshow("FilteredOTSU", filtered)
 
-    plt.subplot(421), plt.imshow(rgb_img)
-    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-    plt.subplot(422), plt.imshow(thresh, 'gray')
-    plt.title("Otsu's binary threshold"), plt.xticks([]), plt.yticks([])
+    hsv_img = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2HSV)
 
-    plt.subplot(423), plt.imshow(closing, 'gray')
-    plt.title("morphologyEx:Closing:2x2"), plt.xticks([]), plt.yticks([])
-    plt.subplot(424), plt.imshow(sure_bg, 'gray')
-    plt.title("Dilation"), plt.xticks([]), plt.yticks([])
+    light_blue = (90, 70, 50)
+    dark_blue = (128, 255, 255)
 
-    plt.subplot(425), plt.imshow(dist_transform, 'gray')
-    plt.title("Distance Transform"), plt.xticks([]), plt.yticks([])
-    plt.subplot(426), plt.imshow(sure_fg, 'gray')
-    plt.title("Thresholding"), plt.xticks([]), plt.yticks([])
+    light_green = (40, 40, 40)
+    dark_green = (70, 255, 255)
 
-    plt.subplot(427), plt.imshow(unknown, 'gray')
-    plt.title("Unknown"), plt.xticks([]), plt.yticks([])
+    light_red = (160, 100, 100)
+    dark_red = (179, 255, 255)
 
-    plt.subplot(428), plt.imshow(img, 'gray')
-    plt.title("Result from Watershed"), plt.xticks([]), plt.yticks([])
-    #
-    # plt.tight_layout()
-    # plt.show()
+    light_red2 = (0, 100, 20)
+    darK_red2 = (10, 255, 255)
 
-    cv2.imshow("wat", img)
+    maskBlue = cv2.inRange(hsv_img, light_blue, dark_blue)
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    maskRed1 = cv2.inRange(hsv_img, light_red, dark_red)
+    maskRed2 = cv2.inRange(hsv_img, light_red2, darK_red2)
+    maskRed = cv2.bitwise_or(maskRed1, maskRed2)
+
+    resultBlue = cv2.bitwise_and(img, img, mask=maskBlue)
+    resultRed = cv2.bitwise_and(img, img, mask=maskRed)
+
+    img_maskBlue_Red = cv2.hconcat([resultRed, resultBlue])
+
+    cv2.imshow("mask Blue Red", img_maskBlue_Red)
+
+    cv2.waitKey()
